4c00fafcb2fce790908a508e1dd382258754f9a1
Miguel Campbell <seanmiguel.campbell@gmail.com>
2024-07-19 19:19:57 -0500

    Added Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { BusinessHomeComponent } from './business-home/business-home.component';
+import { BusinessAppointmentsComponent } from './business-appointments/business-appointments.component';
+import { BusinessAvailabilityComponent } from './business-availability/business-availability.component';
+import { BusinessServicesComponent } from './business-services/business-services.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'business', component: BusinessHomeComponent},
+    {path: 'campaigns/:c_id/:w_id', component: BusinessAppointmentsComponent},
+    {path: 'campaigns/:c_id', component: BusinessAvailabilityComponent},
+    {path: 'campaigns', component: BusinessServicesComponent}
+];
