7a5febabcb68fabe78aa6b3f520ce6679dd8deb9
Miguel Campbell <seanmiguel.campbell@gmail.com>
2024-07-19 19:24:27 -0500

    Redone Routes for Business Components and Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { BusinessHomeComponent } from './business-home/business-home.component';
 import { BusinessAppointmentsComponent } from './business-appointments/business-appointments.component';
 import { BusinessAvailabilityComponent } from './business-availability/business-availability.component';
 import { BusinessServicesComponent } from './business-services/business-services.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 export const routes: Routes = [
+    {path: 'home', component: DashboardComponent},
     {path: 'business', component: BusinessHomeComponent},
-    {path: 'campaigns/:c_id/:w_id', component: BusinessAppointmentsComponent},
-    {path: 'campaigns/:c_id', component: BusinessAvailabilityComponent},
-    {path: 'campaigns', component: BusinessServicesComponent}
+    {path: 'business/appointments', component: BusinessAppointmentsComponent},
+    {path: 'business/availability', component: BusinessAvailabilityComponent},
+    {path: 'business/services', component: BusinessServicesComponent},
+    {path: '', redirectTo: '/home', pathMatch: 'full'}
 ];
